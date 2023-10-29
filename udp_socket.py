4c810d6a21d1fa1b7c08b2daa5c193f3e133dffd
MasterCoookie <jas.kocurek2@wp.pl>
2023-10-29 12:24:30 +0100

    adds basic sending and receiving

--- udp_socket.py
+++ udp_socket.py
@@ -1,9 +1,21 @@
 import socket
 
 class UDPSocket:
-    def __init__(self, out_port, devices_count):
+    def __init__(self, out_port, devices_count, timeout=.5):
         self.out_port = out_port
         self.devices_count = devices_count
 
         self.bound_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.bound_socket.bind(('0.0.0.0', out_port))
+        self.bound_socket.settimeout(timeout)
+
+    def send(self, message_encoded, ip, port, verbose=False):
+        if verbose:
+            print(f"Sending message: {message_encoded.decode()} to {ip}:{port}")
+        self.bound_socket.sendto(message_encoded, (ip, port))
+
+    def receive(self, verbose=False):
+        message_encoded, address = self.bound_socket.recvfrom(1024)
+        if verbose:
+            print(f"Received message: {message_encoded.decode()} from {address}")
+        return message_encoded, address
