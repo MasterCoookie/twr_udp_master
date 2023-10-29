890272cdb097689f3e2e3b800e37d939aac38dfe
MasterCoookie <jas.kocurek2@wp.pl>
2023-10-29 12:54:14 +0100

    adds docstrings to class and method

--- udp_socket.py
+++ udp_socket.py
@@ -1,6 +1,11 @@
 import socket
 
 class UDPSocket:
+    '''
+    Single socket for sending and receiving UDP messages.
+    Only one instance of it should be created in the program.
+    The socket is designed to work in multithreaded environment.
+    '''
     def __init__(self, out_port, devices_count, timeout=.5):
         self.out_port = out_port
         self.devices_count = devices_count
@@ -10,11 +15,22 @@ class UDPSocket:
         self.bound_socket.settimeout(timeout)
 
     def send(self, message_encoded, ip, taget_port, verbose=False):
+        '''
+        Sneds a pre-encoded message to the specified ip and port.
+        message_encoded - bytes
+        ip - string
+        target_port - int
+        '''
         if verbose:
             print(f"Sending message: {message_encoded.decode()} to {ip}:{taget_port}")
         self.bound_socket.sendto(message_encoded, (ip, taget_port))
 
     def receive(self, verbose=False):
+        '''
+        Receives a message from the socket.
+        Returns the encoded message and the address of the sender.
+        If the socket times out, returns None, None.
+        '''
         try:
             message_encoded, address = self.bound_socket.recvfrom(1024)
             if verbose:
