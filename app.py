e888379a257f440fef7ffec01df2d4ad757dee9b
MasterCoookie <jas.kocurek2@wp.pl>
2023-11-04 13:24:42 +0100

    wip Worker

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 import sys
+import time
 
 from queuer import Queuer
 from udp_socket import UDPSocket
+from random_startegy import RandomStrategy
+
+from multiprocessing import Queue, Process, Event
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout
 from PyQt5.QtCore import QThread, QObject, pyqtSignal as Signal, pyqtSlot as Slot
 
+ended = Event()
+ended.clear()
+
 class Worker(QObject):
-    pass
+    msg_q = Queue()
+    result_q = Queue()
+
+    def do_work(self, tags_list):
+        self.queuer = Queuer(tags_list, RandomStrategy())
+        self.udp_socket = UDPSocket(5000, 2)
+
+        p1 = Process(target=self.queuer.queing_process, args=(ended, self.msg_q))
+        p2 = Process(target=self.udp_socket.sending_process, args=(ended, self.msg_q, self.result_q))
+
+        p1.start()
+        # time to prepare first queue
+        time.sleep(.1)
+        p2.start()
+
+        p1.join()
+        p2.join()
+
+        self.udp_socket.bound_socket.close()
 
 class MainWindow(QMainWindow):
     pass
