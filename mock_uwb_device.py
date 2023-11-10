6af789112262fcd81ac18baebbf89f96ec242602
MasterCoookie <jas.kocurek2@wp.pl>
2023-11-10 17:51:48 +0100

    adds mock

---
+++ mock_uwb_device.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+import time
+
+from helper_functions import *
+
+from threading import Thread, Event
+from random import randint
+
+def uwb_mock(port, ended, verbose=False):
+    receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    receiver_socket.bind(('127.0.0.1', port))
+    receiver_socket.settimeout(.5)
+
+    while not ended.is_set():
+        try:
+            message_received, address = receiver_socket.recvfrom(1024)
+            if verbose:
+                print(f"Mock on port {port} has received message: {message_received.decode()} from {address}")
+
+            rand_distance_1 = randint(0, 100)
+            rand_distance_2 = randint(0, 100)
+            rand_distance_full = str(rand_distance_1) + "." + str(rand_distance_2) + "m"
+
+            time.sleep(randint(0, 50) / 1000)
+
+            receiver_socket.sendto(f'DIST: {rand_distance_full}'.encode(), address)
+        except socket.timeout:
+            if verbose:
+                print("Helpers: Receiver socket timeout!")
+
+    receiver_socket.close()
+
+if __name__ == "__main__":
+    count = int(sys.argv[1])
+    ended = Event()
+    ended.clear()
+
+    threads = []
+    for i in range(count):
+        threads.append(Thread(target=uwb_mock, args=(5000 + i, ended, True)))
+
+    input("Press enter to end")
+
+    ended.set()
+    for thread in threads:
+        thread.join()
