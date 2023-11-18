3cf741cf89db354e69169b2a4cffb27e1f8a6e89
MasterCoookie <jas.kocurek2@wp.pl>
2023-11-18 12:07:34 +0100

    creates files

---
+++ positioning_functions.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def trilaterate_3d_4dists(distances):
+    pass
---
+++ test/test_unit_trilaterate.py
@@ -0,0 +1,15 @@
+import unittest
+
+from positioning_functions import *
+
+
+class TestTrilaterate(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_trilaterate_3d_4dists(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
